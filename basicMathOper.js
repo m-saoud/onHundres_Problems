21f560ba7573cfa7e9cf70cfae29322f58380a30
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-10-20 17:39:06 +0900

    simple math operations

---
+++ basicMathOper.js
@@ -0,0 +1,7 @@
+function mathOpertion(op, num1, num2) {
+  if (op == "+") return num1 + num2;
+  if (op == "-") return num1 - num2;
+  if (op == "/") return num1 / num2;
+  if (op == "*") return num1 * num2;
+}
+console.log(mathOpertion("/", 4, 2));
