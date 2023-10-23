512e54558ca58c034c6e1fc93c2631dd7ddee2bb
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-10-23 23:01:50 +0900

    reomve somthing from str & reguler exprestion

---
+++ removeExc.js
@@ -0,0 +1,18 @@
+function remove(str) {
+    return str.replace(/!/,'');
+}
+  console.log(remove.m("!hi!!!"));
+
+let mySTr = "Aa!B!CcDdE127fGgm%v^$HhJj765";
+let mySTraToz = /[a-zA-Z][^(%)!]/gi;
+
+// console.log(mySTr.match(mySTraToz))
+
+let emai = "o@ 12@fas.com @ I@gmail.net Kkklh123 .com...i.. nfo.. 1@gmail.com  451@gmail.com 1";
+let dot = / \d+@\w+.(com|net)/g; 
+ console.log(emai.match(dot))
+
+let sapam = "spamspam ahspam hkSpam saoud mazen spammazen";
+let spamSerch = /(\bspam)/gi;
+
+// console.log(sapam.match(spamSerch));
