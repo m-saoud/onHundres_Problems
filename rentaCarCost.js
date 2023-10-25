4e7cefd9837e73dd2c33421fc470dcbdd6e5ba1e
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-10-26 00:03:55 +0900

    adjust the rent car cost

--- rentaCarCost.js
+++ rentaCarCost.js
@@ -1,13 +1,13 @@
 function cost(d) {
   let basiPrice = 40;
   if (d <= 5 && d >= 3) {
-    basiPrice = Math.floor(` ${basiPrice * d - 1.2} `);
+    basiPrice = basiPrice * d - basiPrice * d * 0.2;
   } else if (d > 5) {
-    basiPrice = Math.floor(basiPrice * d - 1.5);
+    basiPrice = basiPrice * d - basiPrice * d * 0.5
   } else {
     return basiPrice;
   }
   return basiPrice;
 }
 
-console.log(cost(5));
+console.log(cost(6));
