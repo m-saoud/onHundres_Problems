c9a6d3f6e3993ca6723022d913f560f758851b13
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-10-27 19:06:49 +0900

    shoretcut

---
+++ shortCut.js
@@ -0,0 +1,22 @@
+function shortCut(word) {
+  let vol = /[aeiouAEIOU]/gi;
+  word = word.replace(vol, "");
+
+  return word;
+}
+
+console.log(shortCut("syria"));
+
+function shortCut2(word) {
+  let vol = ["a", "e", "i", "o", "u"];
+  let result = [];
+  for (let i = 0; i < word.length; i++) {
+    if (!word.includes(vol[i])) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+console.log(shortCut("mazein horeabar"));
