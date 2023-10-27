7b2c3d18d8a366cc4f7b065ab9065c7687888f0a
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-10-27 14:49:41 +0900

    doullar .cents

---
+++ doullrs-cent.js
@@ -0,0 +1,5 @@
+function fomtMony(amount) {
+    return `$ ${amount.toFixed(2)}`
+}
+
+console.log(fomtMony(2.7980985))
--- formation-decimal-number.js
+++ formation-decimal-number.js
@@ -1,5 +1,6 @@
 function foDecN(n) {
-  return Number(n.toFixed(2));
+  // return Number(n.toFixed(2));
+  return Math.round(n * 100) / 100;
 }
 
 console.log(foDecN(1.23456));
