708d4670a92ac527df794221f6328a83fa7f450d
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-11-07 22:33:20 +0900

    restore the first 3 numbers

--- findCheldren.js
+++ findCheldren.js
@@ -160,7 +160,138 @@ function correct(str) {
 console.log(correct("sa8oud"));
 
 function correctstring(str) {
-  return str.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S');
+  //   return str.replace(/0/g,'O').replace(/1/g,'I').replace(/5/g,'S');
+  const characters = {
+    1: "i",
+    5: "s",
+    0: "o",
+  };
+  return str.replace(/[150]/g, (chare) => characters[chare]);
 }
 
 console.log(correctstring("5a0ud1"));
+
+function between(a, b) {
+  return Array(b - a - 1)
+    .fill(a)
+    .map((n, i) => i + n);
+}
+
+console.log(between(1, 7));
+
+function between2(a, b) {
+  if (a >= b) {
+    return [];
+  }
+  const result = [];
+  for (let i = a; i <= b; i++) {
+    result.push(i);
+  }
+  return result;
+}
+console.log(between2(1, 7));
+
+function goosfilter(birds) {
+  let goos = ["xxx", "sss", "aaa", "bbb"];
+  let findSame = goos.filter((bird) => {
+    return !birds.includes(bird);
+  });
+  return findSame;
+}
+
+console.log(goosfilter("xxx"));
+
+function sumStr(a, b) {
+  return (Number(a) + Number(b)).toString();
+}
+
+console.log(sumStr("5", "6"));
+
+function arryPow(arr, n) {
+  return arr.length - 1 < n ? -1 : Math.pow(arr[n], n);
+}
+
+console.log(arryPow([1, 7, 2, 4], 1));
+
+function finMulty(int, limit) {
+  let result = [];
+  for (let i = 1; i <= limit; i++) {
+    let mmultiply = int * i;
+    if (mmultiply === limit) {
+      result.push(mmultiply);
+      break;
+    } else {
+      result.push(mmultiply);
+    }
+  }
+
+  return result;
+}
+
+console.log(finMulty(5, 25));
+
+function removDuplicatin(arr) {
+  let uniqArr = [...new Set(arr)];
+  return uniqArr;
+}
+console.log(removDuplicatin([1, 2, 3, 2, 4, 5, 5]));
+
+function genratArry(start, end) {
+  return Array(end - start + 1)
+    .fill()
+    .map((_, i) => start + i);
+}
+console.log(genratArry(1, 10));
+
+function capiFirstLettr(str) {
+  return str.replace(/\b\w/g, (toupper) => toupper.toUpperCase());
+}
+console.log(capiFirstLettr("saoud"));
+
+function flasyValue(arr) {
+  return arr.filter(Boolean);
+}
+
+console.log(flasyValue([0, 1, true, false, 4, 6, -2]));
+
+function reomveNum(atr) {
+  return atr.filter((e) => typeof e === "string");
+}
+console.log(reomveNum([1, -2, 0, "s", 3.5]));
+
+function saHello(name, city, state) {
+  return `Welcome,${name
+    .join(", ")
+    .replace(/, ([^,]+)$/g, ", and $1")
+    .replace(/\b\w/g, (toupper) => toupper.toUpperCase())} to ${city.replace(
+    /\b\w/g,
+    (toupper) => toupper.toUpperCase()
+  )} ${state.replace(/\b\w/g, (toupper) => toupper.toUpperCase())}!`;
+}
+console.log(saHello(["saoud", "joun", "samer"], "damscuse", "syria"));
+
+//name['and' + name-1]
+
+function sort(arr) {
+  let temp = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[j - 1] > arr[j]) {
+        temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sort([1, 3, 5, 2, 8, 7, 9, 10, 4]));
+
+function myFunction(a) {
+  
+  return a.slice(0,3)
+
+}
+
+console.log(myFunction([5,4,3,2,1,0]))
