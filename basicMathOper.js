b3f9887fd3abd2b714c337552df85c19a2dcdabc
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-11-13 16:54:48 +0900

    find smooth num&&LeapYear,moveZeros

--- basicMathOper.js
+++ basicMathOper.js
@@ -112,7 +112,6 @@ const gen = generator(10);
 
 console.log(gen.next().value);
 
-
 function* multiplicationGenerator(a) {
   let b = 1;
 
@@ -130,3 +129,109 @@ const generator2 = multiplicationGenerator(5);
 console.log(generator2.next().value); // Output: "5 x 1 = 5"
 
 // ... and so on
+
+function sortByLanguage(list) {
+  for (let i = 0; i < list.length; i++) {
+    const languageComparison = [...list].sort((a, b) =>
+      a.language.localeCompare(b.language)
+    );
+    if (languageComparison !== 0) {
+      return languageComparison;
+    }
+  }
+  return a.firstName.localeCompare(b.firstName);
+}
+
+//
+console.log(
+  sortByLanguage([
+    {
+      firstName: "Nikau",
+      lastName: "R.",
+      country: "New Zealand",
+      continent: "Oceania",
+      age: 39,
+      language: "Ruby",
+    },
+    {
+      firstName: "Precious",
+      lastName: "G.",
+      country: "South Africa",
+      continent: "Africa",
+      age: 22,
+      language: "JavaScript",
+    },
+    {
+      firstName: "Maria",
+      lastName: "S.",
+      country: "Peru",
+      continent: "Americas",
+      age: 30,
+      language: "C",
+    },
+    {
+      firstName: "Agustin",
+      lastName: "V.",
+      country: "Uruguay",
+      continent: "Americas",
+      age: 19,
+      language: "JavaScript",
+    },
+  ])
+);
+
+function moveZeros(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr.sort((a, b) => {
+      if (a === 0) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+  return arr;
+}
+
+console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+
+function isLeapYear(year) {
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isLeapYear(1800));
+
+
+
+function isSmooth(n) {
+  let factor = 2;
+
+  while (n > 1) {
+    while (n % factor === 0) {
+      n /= factor;
+    }
+    factor++;
+  }
+
+  if (factor - 1 === 2) {
+    return "power of 2";
+  } else if (factor - 1 === 3) {
+    return "3-smooth";
+  } else if (factor - 1 === 5) {
+    return "Hamming number";
+  } else if (factor - 1 === 7) {
+    return "humble number";
+  } else {
+    return "non-smooth";
+  }
+}
+
+console.log(isSmooth(16)); // "power of 2"
+console.log(isSmooth(36)); // "3-smooth"
+console.log(isSmooth(60)); // "Hamming number"
+console.log(isSmooth(98)); // "humble number"
+console.log(isSmooth(230)); // "non-smooth"
+
